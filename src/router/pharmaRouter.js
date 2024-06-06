cb21a872a9f4ca6ee66ff93d1270053c3105d9f4
arhamabeer <arhamabeerahmed@gmail.com>
2024-06-06 15:03:11 +0500

    feat: add med router

--- src/router/pharmaRouter.js
+++ src/router/pharmaRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { AddSalt } = require("../controllers/pharma");
+const { AddSalt, AddMedicine } = require("../controllers/pharma");
 const router = express.Router();
 
 router.post("/AddSalt", AddSalt);
+router.post("/AddMedicine", AddMedicine);
 
 module.exports = router;
